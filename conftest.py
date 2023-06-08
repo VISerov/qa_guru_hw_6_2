198be458762f796b0e3a14cf8c1b81e0deb95111
VISerov <serov.qa@gmail.com>
2023-06-08 10:12:13 +0300

    browser.config.driver_name = 'chrome' перенесен в фикстуру.

--- conftest.py
+++ conftest.py
@@ -1,8 +1,9 @@
 import pytest
 
 from selene import browser
-browser.config.driver_name = 'chrome'
+
 @pytest.fixture()
 def size_window():
+    browser.config.driver_name = 'chrome'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
